a3cc43698bdb9ff96b4d4931d8d68cb403abc8b5
QuantumTacoNinja <worapat1h@gmail.com>
2025-01-24 08:45:28 +0700

    update poorly create cluster

--- create_cruster.py
+++ create_cruster.py
@@ -15,13 +15,13 @@ def generate_around_centers(centers: np.array, num_avg: int, distance: float) ->
     all_clusters = []
 
     for center in centers:
-        num_var = np.random.randint(0, num_left // 2 + 1)
+        num_var = np.random.randint(0, num_left // np.e)
         num_left -= num_var
-
+        dist_err = np.random.uniform(0.5, 2*np.e)
         points = []
         while len(points) < num_var:
-            x_i = np.random.uniform(center[0] - distance, center[0] + distance, num_var)
-            y_i = np.random.uniform(center[1] - distance, center[1] + distance, num_var)
+            x_i = np.random.uniform(center[0] - distance * dist_err, center[0] + distance * dist_err, num_var)
+            y_i = np.random.uniform(center[1] - distance * dist_err, center[1] + distance * dist_err, num_var)
 
             dist_sq = (x_i - center[0]) ** 2 + (y_i - center[1]) ** 2
             valid_points = (dist_sq <= distance ** 2)
@@ -49,12 +49,18 @@ def generate_around_centers(centers: np.array, num_avg: int, distance: float) ->
 
     return all_clusters
            
+def create_cluster(num_centers: int, num_points: int, min_v: float, max_v: float, distance: float) -> np.array:
+    return generate_around_centers(centers=choose_centers(min=min_v, max=max_v, num=num_centers), num_avg=num_points, distance=distance)
+
+
 
 if __name__ == "__main__":
-    centers = choose_centers(0, 100, 3)
+    centers = choose_centers(0, 100, 4)
     print(centers)
 
-    clusters = generate_around_centers(centers=centers, num_avg=100, distance=10)
+    clusters = generate_around_centers(centers=centers, num_avg=300, distance=30)
+
+    clusters = create_cluster(num_centers=4, num_points=300, min_v=0, max_v=100, distance=30)
 
     plt.scatter(clusters[:, 0], clusters[:, 1])
     plt.show()
